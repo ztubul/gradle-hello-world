907e29df47607ef5aff54e78efda6340b0e234d4
Anton Weiss <ant.weiss@gmail.com>
2017-12-25 14:13:37 +0200

    Add a test

--- src/main/java/link/otomato/tutorials/HelloGradle.java
+++ src/main/java/link/otomato/tutorials/HelloGradle.java
@@ -4,6 +4,21 @@ public class HelloGradle{
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello Gradle!" );
+        String name = "";
+        if (args.length > 0)
+        {
+            name = capitalize(args[0]);
+        }
+        else
+        {
+            name = "Gradle";
+        }
+        System.out.println("Hello " + name +"!");
+    }
+
+    public static String capitalize( String name )
+    {
+        name = name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
+        return name;
     }
 }
---
+++ src/test/java/link/otomato/tutorials/capTest.java
@@ -0,0 +1,10 @@
+package link.otomato.tutorials;
+import org.junit.jupiter.api.Test;
+
+class capTest {
+
+    @Test
+    void capTest() {
+        assert HelloGradle.capitalize("nAMe") == "Name";
+    }
+}
